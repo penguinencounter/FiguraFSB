5bae29c037321c6bbe8250946bbc17cc2aa971ff
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2026-05-24 20:04:16 -0700

    fixed the wtf

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 val modVersion: String by project
 project.version = modVersion
-val group: String by project
-project.group = group
+val rootGroup: String by project
+project.group = rootGroup
